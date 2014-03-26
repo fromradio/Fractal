2edb2b520328f0742f8edef6ee7093a060b7ee1b
fromradio <ruimin.wang13@gmail.com>
2014-03-27 00:42:51 +0800

    code modified

--- code/fractal.py
+++ code/fractal.py
@@ -2,7 +2,9 @@
 import numpy as np
 import scipy as sp
 import cv2
-
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib import cm
 
 class fractalImage:
     """
@@ -16,25 +18,52 @@ class fractalImage:
         self.ylen = ylen
         self.width = width
         self.height = height
+        
+        self.para = [30,1e20]
+        if para:
+            self.para = para# iteration and the radius
         self.imageInit()
-        if not para:
-            self.para = [10,1000]# iteration and the radius
+        
     def imageInit(self):
-        self.comArray = np.zeros((self.width,self.height),dtype=np.complex64)
+        self.comArray = np.zeros((self.width,self.height),dtype=np.complex128)
         xlin = np.linspace(0,self.xlen,self.width)
         ylin = np.linspace(0,self.ylen,self.height)
         xv,yv = np.meshgrid(xlin,ylin)
         #xinterval = float(self.xlen)/(self.width-1)
         #yinterval = float(self.ylen)/(self.height-1)
         self.comArray = self.origin[0]+1j*self.origin[1]+xv+1j*yv
-        print self.comArray.shape
-        print self.comArray
+        Z = 0*self.comArray
+        count = np.zeros((self.width,self.height),dtype = np.int8)
+        judgeArray = np.ones((self.width,self.height),dtype = np.bool)
+        
+        for i in range(0,self.para[0]):
+            Z[judgeArray]  =  Z[judgeArray]*Z[judgeArray]+self.comArray[judgeArray]
+            func = abs(Z)
+            #testF = np.where(func>self.para[1])
+            #print testF
+            
+            count[(judgeArray)&(func>self.para[1])] = i
+            judgeArray[func>self.para[1]] = False
+            func[func>self.para[1]] = 0
+        #func = abs(Z)
+        #print func[func<self.para[1]]
+        #print func
+        #func[func<self.para[1]] = 0
+        self.toImage(count)
+        #print func
+        #print self.comArray
+    def toImage(self,func):
+        # do log
+        print func
+        func[func>self.para[1]] = np.log10(func[func>self.para[1]])
+        plt.pcolor(func,cmap = cm.cool)
+        plt.colorbar()
+        plt.show()
     def computeFractal(self):
         pass
-        
 #main function for debug
 def main():
-    fI = fractalImage((-1,-1),2,2,100,100)
+    fI = fractalImage((-1,-1),0.2,0.2,500,500)
 if __name__ == '__main__':
     main()
     
